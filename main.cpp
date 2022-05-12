b6d91816fe47138ea7de57435fac550b38ca38c9
Obuchi Yuki <yukibochi1@gmail.com>
2022-05-12 13:13:04 +0900

    add debug

--- main.cpp
+++ main.cpp
@@ -16,10 +16,16 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    vector<string> args(argv, argv+argc);
     
+#if DEBUG
+    vector<string> args = { "gcc", "/Users/yuki/Developer/Git/ncc/sample.c" };
+    auto logpath = "/Users/yuki/Desktop/ncc.log";
+    auto config = CCConfigFactory::make_debug(logpath);
+#else
+    vector<string> args(argv, argv+argc);
     auto basepath = "/www/nakai.hisashi.gt/Lecture/Programming/log";
     auto config = CCConfigFactory::make_user(basepath);
+#endif
     auto logger = make_shared<CCLogger>(config);
     auto ncc = CCCommand(config, logger, CommandExecutor::shared());
     

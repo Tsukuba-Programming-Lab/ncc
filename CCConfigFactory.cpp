9a6f6498bbb2f812f9755938f205456c519c81f1
Obuchi Yuki <yukibochi1@gmail.com>
2022-05-12 14:07:12 +0900

    add debug

--- CCConfigFactory.cpp
+++ CCConfigFactory.cpp
@@ -5,6 +5,7 @@
 //  Created by yuki on 2022/04/21.
 //
 
+#include <iostream>
 #include "CCConfigFactory.hpp"
 
 auto CCConfigFactory::make_debug(string logfile) -> CCConfig {
@@ -12,8 +13,10 @@ auto CCConfigFactory::make_debug(string logfile) -> CCConfig {
 }
 auto CCConfigFactory::make_user(string basepath) -> CCConfig {
     auto path = basepath;
+    cout << "error" << endl;
     auto filename = string(getlogin()) + ".log";
     path += "/";
     path += filename;
+    cout << "error" << endl;
     return CCConfig{ path, "cc" };
 }
